a007f638ab1c0817a7f293aeace0b1c57893016a
Tarbolin69 <tarbolin69@gmail.com>
2023-03-03 01:48:49 -0300

    Arreglando cosas con org y teclas

--- init.el
+++ init.el
@@ -138,8 +138,6 @@
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
@@ -173,7 +171,8 @@
 
 (angl/leader-keys
    "o" '(:ignore t :which-key "Org Actions")
-   "oA" '(org-agenda :which-key "Open Org Agenda"))
+   "oA" '(org-agenda :which-key "Open Org Agenda")
+   "ot" '(counsel-org-tag :which-key "Counsel Tags"))
 
 (use-package projectile
   :diminish projectile-mode
