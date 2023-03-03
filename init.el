4f31e65843fb733fa0e7295138ac3f5b5bb4e968
Tarbolin69 <tarbolin69@gmail.com>
2023-03-03 01:25:34 -0300

    Mas cosas para org agenda

--- init.el
+++ init.el
@@ -83,7 +83,6 @@
   :config
   (ivy-mode 1))
 
-
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
@@ -131,8 +130,6 @@
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC")
-;;  TODO set a new leader-keys under angl/leaderkeys
-
   (angl/leader-keys
    "t" '(:ignore t :which-key "toggles")
    "tt" '(counsel-load-theme :which-key "choose theme")))
@@ -160,12 +157,11 @@
   :config
   (evil-collection-init))
 
-;; TODO Configure HYDRA
-
+;; ----------------- Configure Hydras later for Git Actions
 (use-package hydra
   :defer t)
 
-(defhydra hydra-text-scale (:timeout 4)
+(defhydra hydra-text-scale (:timeout 1)
   "scale text"
   ("j" text-scale-increase "in")
   ("k" text-scale-decrease "out")
@@ -173,6 +169,11 @@
 
 (angl/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
+;; ----------------- And set them up using leader-keys and such ^^^
+
+(angl/leader-keys
+   "o" '(:ignore t :which-key "Org Actions")
+   "oA" '(org-agenda :which-key "Open Org Agenda"))
 
 (use-package projectile
   :diminish projectile-mode
@@ -213,9 +214,59 @@
   (setq org-log-done 'time) ;; TODO Maybe change it to "note" to add thoughts on stuff in uni
   (setq org-log-into-drawer t)
   (setq org-agenda-files
-	      '("~/Org/Tasks.org"))
+	'("~/Org/Tasks.org"
+	  "~/Org/Cumpleaños.org"))
 	org-hide-emphasis-markers t)
+(setq org-todo-keywords
+    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+      (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+;; Configure custom agenda views TODO Traducir todo esto al español
+  (setq org-agenda-custom-commands
+   '(("d" "Tablero"
+     ((agenda "" ((org-deadline-warning-days 7)))
+      (todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))
+      (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
 
+    ("n" "Next Tasks"
+     ((todo "NEXT"
+        ((org-agenda-overriding-header "Next Tasks")))))
+
+    ("W" "Work Tasks" tags-todo "+work-email")
+
+    ;; Low-effort next actions
+    ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+     ((org-agenda-overriding-header "Low Effort Tasks")
+      (org-agenda-max-todos 20)
+      (org-agenda-files org-agenda-files)))
+
+    ("w" "Workflow Status"
+     ((todo "WAIT"
+            ((org-agenda-overriding-header "Waiting on External")
+             (org-agenda-files org-agenda-files)))
+      (todo "REVIEW"
+            ((org-agenda-overriding-header "In Review")
+             (org-agenda-files org-agenda-files)))
+      (todo "PLAN"
+            ((org-agenda-overriding-header "In Planning")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "BACKLOG"
+            ((org-agenda-overriding-header "Project Backlog")
+             (org-agenda-todo-list-sublevels nil)
+             (org-agenda-files org-agenda-files)))
+      (todo "READY"
+            ((org-agenda-overriding-header "Ready for Work")
+             (org-agenda-files org-agenda-files)))
+      (todo "ACTIVE"
+            ((org-agenda-overriding-header "Active Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "COMPLETED"
+            ((org-agenda-overriding-header "Completed Projects")
+             (org-agenda-files org-agenda-files)))
+      (todo "CANC"
+            ((org-agenda-overriding-header "Cancelled Projects")
+             (org-agenda-files org-agenda-files)))))))
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
   :custom
